238b0d8051dd33ba2b1ba203535e67172e1b9bfc
dhaval <dhavalchauhan7979@gmail.com>
2026-07-06 20:43:21 +0530

    fix: sanitize firebase storage bucket name

--- src/config/env.js
+++ src/config/env.js
@@ -29,7 +29,9 @@ const env = {
     privateKey: process.env.FIREBASE_PRIVATE_KEY
       ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
       : undefined,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET
+      ? process.env.FIREBASE_STORAGE_BUCKET.replace(/^gs:\/\//i, '').replace(/\/+$/, '').trim()
+      : undefined,
   },
 
   // File uploads
